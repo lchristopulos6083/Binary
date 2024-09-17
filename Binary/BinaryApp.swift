5c83b633056e9405672c7df0d0842efced36a49a
Luke Christopulos <lukechristopulos@gmail.com>
2024-09-17 08:57:59 -0500

    Initial Commit

---
+++ Binary/BinaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinaryApp.swift
+//  Binary
+//
+//  Created by Luke R. Christopulos on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct BinaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
