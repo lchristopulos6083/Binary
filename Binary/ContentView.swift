bd751e198f2ab31b2d60765e3ffbb4f93300ed90
Luke Christopulos <lukechristopulos@gmail.com>
2024-09-19 08:22:37 -0500

    textbox

--- Binary/ContentView.swift
+++ Binary/ContentView.swift
@@ -8,17 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    
+    @State private var baseNumber: Int = 0
+        @State private var baseNumberString: String = "0"
+        var body: some View {
+            VStack {
+                TextField("Enter a number", text: $baseNumberString)
+                    .keyboardType(.numberPad)
+                    .onChange(of: baseNumberString) { newValue in
+                        if let newNumber = Int(newValue) {
+                            baseNumber = newNumber
+                        } else {
+                            baseNumber = 0
+                        }
+                    }
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                Text("Base Number: \(baseNumber)")
+                    .padding()
+            }
+            .padding()
+            .onAppear {
+                baseNumberString = "\(baseNumber)"
+            }
         }
-        .padding()
     }
-}
 
-#Preview {
-    ContentView()
-}
+    #Preview {
+        ContentView()
+    }
